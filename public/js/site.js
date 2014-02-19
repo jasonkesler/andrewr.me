67973fa7295bd95cbf6baf44d766f77cd2e16cec
Andrew Richardson <andrewr114@gmail.com>
2014-02-19 14:20:20 -0800

    Added jQuery and a basic script for pinning menu to the top on scroll

---
+++ public/js/site.js
@@ -0,0 +1,33 @@
+const NAV_TOP_PADDING = 20;
+
+var sidebarScrollTop = 0;
+
+function updateSidebarScrollTop() {
+    sidebarScrollTop = $(".sidebar-nav").offset().top;
+}
+
+function updateNavPosition() {
+    var docScrollTop = $('body,html').scrollTop();
+    if(docScrollTop > sidebarScrollTop - NAV_TOP_PADDING) {
+        $(".sidebar-nav").css({position: 'fixed', top: NAV_TOP_PADDING + 'px'});
+    } else {
+        $(".sidebar-nav").css({position: 'static'});
+    }
+};
+
+$(document).ready(function() {
+    updateSidebarScrollTop();
+    $(window).scroll(function() { 
+        updateNavPosition();
+    });
+});
+
+$(window).resize(function() {
+    updateSidebarScrollTop();
+    updateNavPosition();
+});
+
+// $(document).resize(function() {
+//     updateSidebarScrollTop();
+//     updateNavPosition();
+// });
