6267890f798d4da2bb6283e7113b5783ac950bc0
Andrew Richardson <andrewr114@gmail.com>
2014-02-20 13:05:30 -0800

    - Better mobile layout - Added profile picture in About page - Added social links / icons - Added project page - Cleanup

--- _assets/js/site.js
+++ _assets/js/site.js
@@ -1,33 +1,33 @@
 //= require vendor/jquery
 
-const NAV_TOP_PADDING = 20;
+// const NAV_TOP_PADDING = 20;
 
-var sidebarScrollTop = 0;
+// var sidebarScrollTop = 0;
 
-function updateSidebarScrollTop() {
-    sidebarScrollTop = $(".sidebar-nav").offset().top;
-}
+// function updateSidebarScrollTop() {
+//     sidebarScrollTop = $(".sidebar-nav").offset().top;
+// }
 
-function updateNavPosition() {
-    var docScrollTop = $('body,html').scrollTop();
-    if(docScrollTop > sidebarScrollTop - NAV_TOP_PADDING) {
-        $(".sidebar-nav").css({position: 'fixed', top: NAV_TOP_PADDING + 'px'});
-    } else {
-        $(".sidebar-nav").css({position: 'static'});
-    }
-};
+// function updateNavPosition() {
+//     var docScrollTop = $('body,html').scrollTop();
+//     if(docScrollTop > sidebarScrollTop - NAV_TOP_PADDING) {
+//         $(".sidebar-nav").css({position: 'fixed', top: NAV_TOP_PADDING + 'px'});
+//     } else {
+//         $(".sidebar-nav").css({position: 'static'});
+//     }
+// };
 
-$(document).ready(function() {
-    updateSidebarScrollTop();
-    $(window).scroll(function() { 
-        updateNavPosition();
-    });
-});
+// $(document).ready(function() {
+//     updateSidebarScrollTop();
+//     $(window).scroll(function() { 
+//         updateNavPosition();
+//     });
+// });
 
-$(window).resize(function() {
-    updateSidebarScrollTop();
-    updateNavPosition();
-});
+// $(window).resize(function() {
+//     updateSidebarScrollTop();
+//     updateNavPosition();
+// });
 
 // $(document).resize(function() {
 //     updateSidebarScrollTop();
--- about.md
+++ about.md
@@ -3,6 +3,8 @@ layout: page
 title: About
 ---
 
+<div class="profile-pic"></div>
+
 I’m Andrew Richardson, and I’m an iOS developer, UI designer, and student living in Vancouver, BC.
 
 I’ve been developing for iOS for over two years, working primarily outside the bounds of the App Store to change the way iOS works for the better. My goal is to create elegant and innovative interface solutions to complex problems. Working with talented designers as well as with designs of my own, I love to take these designs and make them a reality. This can be seen in my flagship projects Jukebox and Merge, with more ambitious projects on the way.
